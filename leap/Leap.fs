0ffbba259a706026c4c5ccdb08e326e2c8c66ca7
larissagarciacbl <larissa.garcia@coolblue.nl>
2019-05-03 15:33:12 +0200

    Solving leap problem

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,3 +1,8 @@
 ﻿module Leap
 
-let leapYear (year: int): bool = failwith "implement the function"
+let leapYear (year: int): bool = 
+    let divisibleByFour = year%4 = 0
+    let divisibleByOneHundred = year%100 = 0
+    let divisibleByFourHundred = year%400 = 0
+
+    divisibleByFour && (not divisibleByOneHundred || divisibleByFourHundred);
